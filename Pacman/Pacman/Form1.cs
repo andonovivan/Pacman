f311dc0c161ea49d1af016e472e7444dd219003c
Ivan Andonov <ivan.andonovv@hotmail.com>
2018-05-18 15:49:13 +0200

    Adding Pacman

---
+++ Pacman/Pacman/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Pacman.Properties;
+
+namespace Pacman
+{
+    public sealed partial class Form1 : Form
+    {
+        private Timer _timer;
+        private Pacman _pacman;
+        private const int TimerInterval = 300;
+        private const int WorldWidth = 15;
+        private const int WorldHeight = 10;
+        private readonly Image _foodImage;
+        private bool[][] _foodWorld;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _foodImage = Resources.Pacman_peach_sh;
+            NewGame();
+            DoubleBuffered = true;
+        }
+
+        public void NewGame()
+        {
+            _pacman = new Pacman();
+            Width = Pacman.Radius * 2 * (WorldWidth + 1);
+            Height = Pacman.Radius * 2 * (WorldHeight + 1);
+            _foodWorld = new bool[30][];
+            for (var i = 0; i < _foodWorld.Length; i++)
+            {
+                _foodWorld[i] = new bool[WorldWidth];
+                for (var j = 0; j < _foodWorld[i].Length; j++)
+                {
+                    _foodWorld[i][j] = true;
+                }
+            }
+            _timer = new Timer();
+            _timer.Tick +=timer_Tick;
+            _timer.Interval = TimerInterval;
+            _timer.Start();
+          
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            for (var i = 0; i < _foodWorld.Length; i++)
+            {
+                for (var j = 0; j < _foodWorld[i].Length; j++)
+                {
+                    if (_pacman.X == j && _pacman.Y == i)
+                    {
+                        _foodWorld[i][j] = false;
+                    }
+                }
+            }
+            _pacman.Move(WorldWidth, WorldHeight);
+            Invalidate();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    _pacman.ChangeDirection(Pacman.Direction.Up);
+                    break;
+                case Keys.Down:
+                    _pacman.ChangeDirection(Pacman.Direction.Down);
+                    break;
+                case Keys.Right:
+                    _pacman.ChangeDirection(Pacman.Direction.Right);
+                    break;
+                case Keys.Left:
+                    _pacman.ChangeDirection(Pacman.Direction.Left);
+                    break;
+            }
+
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+            for (var i = 0; i < _foodWorld.Length; i++)
+            {
+                for (var j = 0; j < _foodWorld[i].Length; j++)
+                {
+                    if (_foodWorld[i][j])
+                    {
+                        e.Graphics.DrawImageUnscaled(_foodImage, j * Pacman.Radius * 2 + (Pacman.Radius * 2 - _foodImage.Height) / 2, i * Pacman.Radius * 2 + (Pacman.Radius * 2 - _foodImage.Width) / 2);
+                    }
+                }
+            }
+            _pacman.Draw(e.Graphics);
+        }
+    }   
+}
---
+++ Pacman/Pacman/Pacman.cs
@@ -0,0 +1,101 @@
+﻿using System.Drawing;
+
+namespace Pacman
+{
+    public class Pacman
+    {
+        public enum Direction
+        {
+            Right,
+            Left,
+            Up,
+            Down
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        private readonly Brush _brush;
+        private Direction _direction;
+        public static readonly int Radius = 20;
+        private bool _isOpen;
+
+        public Pacman()
+        {
+            X = 7;
+            Y = 5;
+            _direction = Direction.Right;
+            _brush = new SolidBrush(Color.Yellow);
+        }
+
+        public void ChangeDirection(Direction direction)
+        {
+            _direction = direction;
+        }
+
+        public void Move(int width, int height)
+        {
+            switch (_direction)
+            {
+                case Direction.Right:
+                    X += 1;
+                    if (X >= width)
+                    {
+                        X = 0;
+                    }
+
+                    break;
+                case Direction.Left:
+                    X -= 1;
+                    if (X < 0)
+                    {
+                        X = width - 1;
+                    }
+
+                    break;
+                case Direction.Up:
+                    Y -= 1;
+                    if (Y < 0)
+                    {
+                        Y = height - 1;
+                    }
+
+                    break;
+                case Direction.Down:
+                    Y += 1;
+                    if (Y >= height)
+                    {
+                        Y = 0;
+                    }
+
+                    break;
+            }
+            _isOpen = !_isOpen;
+        }
+
+        public void Draw(Graphics g)
+        {
+            if (!_isOpen)
+            {
+                g.FillEllipse(_brush, X * 2 * Radius, Y * 2 * Radius, Radius * 2, Radius * 2);
+            }
+            else
+            {
+                switch (_direction)
+                {
+                    case Direction.Right:
+                        g.FillPie(_brush, X * 2 * Radius, Y * 2 * Radius, Radius * 2, Radius * 2, 45, 270);
+                        break;
+                    case Direction.Left:
+                        g.FillPie(_brush, X * 2 * Radius, Y * 2 * Radius, Radius * 2, Radius * 2, 225, 270);
+                        break;
+                    case Direction.Up:
+                        g.FillPie(_brush, X * 2 * Radius, Y * 2 * Radius, Radius * 2, Radius * 2, 315, 270);
+                        break;
+                    case Direction.Down:
+                        g.FillPie(_brush, X * 2 * Radius, Y * 2 * Radius, Radius * 2, Radius * 2, 135, 270);
+                        break;
+                }
+            }
+        }
+    }
+}
